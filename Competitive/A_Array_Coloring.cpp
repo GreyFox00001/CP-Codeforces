f653c4b6558d06536ddb3d76f320278a151c00f8
Tanish Kohli <162027723+GreyFox00001@users.noreply.github.com>
2025-01-03 14:24:44 +0530

    add

--- Competitive/A_Array_Coloring.cpp
+++ Competitive/A_Array_Coloring.cpp
@@ -1,2 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            sum+=arr[i];
+        }
+        int a=0;
+        bool yes =0;
+        for(int i =0;i<n;i++){
+            a+=arr[i];
+            sum -=arr[i];
+            if(sum%2 == a%2){
+                cout<<"YES"<<endl;
+                yes = 1;
+                break;
+            }
+        }
+        if(yes == 0){
+            cout<<"NO"<<endl;
+        }
+    }
+}
